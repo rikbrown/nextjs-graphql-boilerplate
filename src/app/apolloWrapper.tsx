93b34fc269c51de939d4abb0f340152c7456658a
Rik Brown <rik@rik.codes>
2023-06-13 16:01:38 +0100

    Prefix https

--- src/app/apolloWrapper.tsx
+++ src/app/apolloWrapper.tsx
@@ -7,7 +7,7 @@ import {
   SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support/ssr"
 
-const endpoint = process.env.VERCEL_URL ?? "http://localhost:3000"
+const endpoint = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 function makeClient() {
   const httpLink = new HttpLink({
